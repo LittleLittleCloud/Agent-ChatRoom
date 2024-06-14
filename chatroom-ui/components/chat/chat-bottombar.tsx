a415d97a66b755f093eb7b50a8030cc7372cfb3d
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-06-14 13:46:21 -0700

    update

--- chatroom-ui/components/chat/chat-bottombar.tsx
+++ chatroom-ui/components/chat/chat-bottombar.tsx
@@ -1,85 +1,94 @@
 import {
-    FileImage,
-    Mic,
-    Paperclip,
-    PlusCircle,
-    SendHorizontal,
-    Smile,
-    ThumbsUp,
-  } from "lucide-react";
-  import Link from "next/link";
-  import React, { useRef, useState } from "react";
-  import { buttonVariants } from "../ui/button";
-  import { cn } from "@/lib/utils";
-  import { AnimatePresence, motion } from "framer-motion";
-  import { Message, loggedInUserData } from "@/types/Message";
-  import { Textarea } from "@/components/ui/textarea";
-  import { EmojiPicker } from "@/components/emoji-picker";
-  import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+  FileImage,
+  Mic,
+  Paperclip,
+  PlusCircle,
+  SendHorizontal,
+  Smile,
+  ThumbsUp,
+} from "lucide-react";
+import Link from "next/link";
+import React, { useRef, useState } from "react";
+import { buttonVariants } from "../ui/button";
+import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "framer-motion";
+import { Message, loggedInUserData } from "@/types/Message";
+import { Textarea } from "@/components/ui/textarea";
+import { EmojiPicker } from "@/components/emoji-picker";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { AgentInfo, ChatMsg } from "@/chatroom-client";
-  
-  interface ChatBottombarProps {
-    user: AgentInfo
-    sendMessage: (newMessage: ChatMsg) => void;
-    isMobile: boolean;
-  }
-  
-  // export const BottombarIcons = [{icon: PlusCircle}, { icon: FileImage }, { icon: Paperclip }];
-  
-  export default function ChatBottombar({
-    sendMessage,
-    isMobile,
-    user,
-  }: ChatBottombarProps) {
-    const [message, setMessage] = useState("");
-    const inputRef = useRef<HTMLTextAreaElement>(null);
-  
-    const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-      setMessage(event.target.value);
+
+interface ChatBottombarProps {
+  user: AgentInfo
+  sendMessage: (newMessage: ChatMsg) => void;
+  isMobile: boolean;
+}
+
+// export const BottombarIcons = [{icon: PlusCircle}, { icon: FileImage }, { icon: Paperclip }];
+
+export default function ChatBottombar({
+  sendMessage,
+  isMobile,
+  user,
+}: ChatBottombarProps) {
+  const [message, setMessage] = useState("");
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+  const [rowNumber, setRowNumber] = useState(1);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const rows = event.target.value.split("\n").length;
+    setRowNumber(rows);
+    setMessage(event.target.value);
+  };
+
+  const handleThumbsUp = () => {
+    const newMessage: ChatMsg = {
+      from: user.name,
+      text: "👍",
+      created: new Date().toISOString(),
     };
-  
-    const handleThumbsUp = () => {
+    sendMessage(newMessage);
+    setMessage("");
+  };
+
+  const handleSend = () => {
+    if (message.trim()) {
       const newMessage: ChatMsg = {
         from: user.name,
-        text: "👍",
+        text: message.trim(),
         created: new Date().toISOString(),
       };
       sendMessage(newMessage);
       setMessage("");
-    };
-  
-    const handleSend = () => {
-      if (message.trim()) {
-        const newMessage: ChatMsg = {
-          from: user.name,
-          text: message.trim(),
-          created: new Date().toISOString(),
-        };
-        sendMessage(newMessage);
-        setMessage("");
-  
-        if (inputRef.current) {
-          inputRef.current.focus();
-        }
+
+      if (inputRef.current) {
+        inputRef.current.focus();
       }
-    };
-  
-    const handleKeyPress = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-      if (event.key === "Enter" && !event.shiftKey) {
-        event.preventDefault();
-        handleSend();
-      }
-  
-      if (event.key === "Enter" && event.shiftKey) {
-        event.preventDefault();
-        setMessage((prev) => prev + "\n");
-      }
-    };
-  
-    return (
-      <div className="p-2 flex justify-between w-full items-center gap-2">
-        <div className="flex">
-            {/* <Popover>
+    }
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      handleSend();
+    }
+
+    if (event.key === "Enter" && event.shiftKey) {
+      // var cursorPosition = event.currentTarget.selectionStart;
+      // setMessage((prev) => {
+      //   return (
+      //     prev.substring(0, cursorPosition) +
+      //     "\n" +
+      //     prev.substring(cursorPosition)
+      //   );
+      // });
+    }
+  };
+
+  return (
+    <div className="p-2 flex justify-between w-full items-center gap-2">
+      <div className="flex">
+        {/* <Popover>
               <PopoverTrigger asChild>
               <Link
             href="#"
@@ -121,7 +130,7 @@ import { AgentInfo, ChatMsg } from "@/chatroom-client";
                )}
               </PopoverContent>
             </Popover> */}
-          {/* {!message.trim() && !isMobile && (
+        {/* {!message.trim() && !isMobile && (
             <div className="flex">
               {BottombarIcons.map((icon, index) => (
                 <Link
@@ -138,12 +147,14 @@ import { AgentInfo, ChatMsg } from "@/chatroom-client";
               ))}
             </div>
           )} */}
-        </div>
-  
+      </div>
+
+      <div
+        className="w-full h-full  relative bg-none flex items-center border-accent border-solid rounded-md overflow-y-auto">
         <AnimatePresence initial={false}>
           <motion.div
             key="input"
-            className="w-full relative"
+            className="w-full relative flex items-center gap-2 pl-2"
             layout
             initial={{ opacity: 0, scale: 1 }}
             animate={{ opacity: 1, scale: 1 }}
@@ -156,52 +167,62 @@ import { AgentInfo, ChatMsg } from "@/chatroom-client";
               },
             }}
           >
-            <Textarea
-              autoComplete="off"
-              value={message}
-              ref={inputRef}
-              onKeyDown={handleKeyPress}
-              onChange={handleInputChange}
-              name="message"
-              placeholder="Aa"
-              className="w-full border rounded-full flex items-center h-9 resize-none overflow-hidden bg-background"
-            ></Textarea>
-            <div className="absolute right-2 bottom-0.5  ">
+            <div
+              className={cn(
+                "flex w-full p-0 border-2 border-solid border-accent rounded-md",
+                rowNumber > 1 ? "flex-col" : ""
+              )}>
+              <Textarea
+                autoComplete="off"
+                value={message}
+                ref={inputRef}
+                rows={rowNumber}
+                onKeyDown={handleKeyPress}
+                onChange={handleInputChange}
+                name="message"
+                placeholder="Aa"
+                className={
+                  cn("h-full border-none flex items-center overflow-hidden bg-background ring-0 focus:ring-0 focus:outline-none resize-none p-2",
+                  )} />
+              
+
+              <div className="flex gap-3 justify-end items-center">
               <EmojiPicker onChange={(value) => {
                 setMessage(message + value)
                 if (inputRef.current) {
                   inputRef.current.focus();
                 }
               }} />
+                {message.trim() ? (
+                  <Link
+                    href="#"
+                    className={cn(
+                      buttonVariants({ variant: "ghost", size: "icon" }),
+                      "w-12 h-9",
+                      "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0"
+                    )}
+                    onClick={handleSend}
+                  >
+                    <SendHorizontal size={20} className="text-muted-foreground" />
+                  </Link>
+                ) : (
+                  <Link
+                    href="#"
+                    className={cn(
+                      buttonVariants({ variant: "ghost", size: "icon" }),
+                      "w-12 h-9",
+                      "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0"
+                    )}
+                    onClick={handleThumbsUp}
+                  >
+                    <ThumbsUp size={20} className="text-muted-foreground" />
+                  </Link>
+                )}
+              </div>
             </div>
           </motion.div>
-  
-          {message.trim() ? (
-            <Link
-              href="#"
-              className={cn(
-                buttonVariants({ variant: "ghost", size: "icon" }),
-                "h-9 w-9",
-                "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0"
-              )}
-              onClick={handleSend}
-            >
-              <SendHorizontal size={20} className="text-muted-foreground" />
-            </Link>
-          ) : (
-            <Link
-              href="#"
-              className={cn(
-                buttonVariants({ variant: "ghost", size: "icon" }),
-                "h-9 w-9",
-                "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0"
-              )}
-              onClick={handleThumbsUp}
-            >
-              <ThumbsUp size={20} className="text-muted-foreground" />
-            </Link>
-          )}
         </AnimatePresence>
       </div>
-    );
-  }
+    </div>
+  );
+}
--- chatroom-ui/components/ui/textarea.tsx
+++ chatroom-ui/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex w-full border resize-auto border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
