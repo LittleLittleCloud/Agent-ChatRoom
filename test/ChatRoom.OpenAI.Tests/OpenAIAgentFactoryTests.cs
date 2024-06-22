28f32c2f67c4ac9f0b3dbec79900f2abcb222b00
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-06-22 12:37:40 -0700

    add error message middleware

---
+++ test/ChatRoom.OpenAI.Tests/OpenAIAgentFactoryTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoGen.Core;
+using FluentAssertions;
+using Xunit;
+
+namespace ChatRoom.OpenAI.Tests;
+
+public class OpenAIAgentFactoryTests
+{
+    [Fact]
+    public async Task ItReturnDefaultReplyAgentWhenConfigurationIsNotValid()
+    {
+        var configuration = new OpenAIAgentConfiguration();
+        configuration.LLMConfiguration.AzureOpenAIKey = null;
+        var agentFactory = new OpenAIAgentFactory(configuration);
+        var agent = agentFactory.CreateAgent();
+        agent.Should().BeOfType<DefaultReplyAgent>();
+    }
+
+    [Fact]
+    public async Task ItReturnErrorMessageWhenMeetException()
+    {
+        var configuration = new OpenAIAgentConfiguration();
+        configuration.LLMConfiguration.AzureOpenAIKey = "invalid key";
+        var agentFactory = new OpenAIAgentFactory(configuration);
+        var agent = agentFactory.CreateAgent();
+
+        var reply = await agent.SendAsync("hey");
+        // should get 401 error
+        reply.GetContent().Should().Contain("401");
+    }
+}
