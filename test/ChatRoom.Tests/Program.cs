9d5a5a0ce29c3f94a5de3c37163ca57f9d07c767
Xiaoyun Zhang <xiaoyuz@microsoft.com>
2024-06-23 13:44:55 -0700

    update (#77)

---
+++ test/ChatRoom.Tests/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Management.Automation;
+using ChatRoom.BingSearch.Tests;
+using ChatRoom.Client.Tests;
+using ChatRoom.Github.Tests;
+using ChatRoom.OpenAI.Tests;
+using ChatRoom.Powershell.Tests;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using var defaultClientFixture = new DefaultClientFixture();
+using var openaiFixture = new OpenAIAgentsFixture();
+using var bing = new BingSearchFixture();
+using var githubFixture = new GithubAgentsFixture();
+using var psFixture = new PowershellAgentsFixture();
+using var host = Host.CreateDefaultBuilder()
+    .Build();
+
+var applicationLifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
+
+applicationLifetime.ApplicationStopping.Register(() =>
+{
+    Console.WriteLine("Application is stopping.");
+});
+
+await host.StartAsync();
+await host.WaitForShutdownAsync();
