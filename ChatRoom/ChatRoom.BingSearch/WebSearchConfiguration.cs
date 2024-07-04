f89fdffa1b5774a8fe5b2565768272779aed7099
Xiaoyun Zhang <xiaoyuz@microsoft.com>
2024-07-03 23:05:08 -0700

    add web search api (#89)

---
+++ ChatRoom/ChatRoom.BingSearch/WebSearchConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using ChatRoom.SDK;
+using Json.Schema.Generation;
+using System.Text.Json.Serialization;
+
+namespace ChatRoom.WebSearch;
+
+public class WebSearchConfiguration
+{
+    [JsonPropertyName("bing_search_config")]
+    [Description("The configuration for Bing search agent")]
+    public BingSearchConfiguration? BingSearchConfiguration { get; set; }
+
+    [JsonPropertyName("google_search_config")]
+    [Description("The configuration for Google search agent")]
+    public GoogleSearchConfiguration? GoogleSearchConfiguration { get; set; }
+
+    [JsonPropertyName("room_config")]
+    [Description("The configuration for the chat room")]
+    public RoomConfiguration RoomConfig { get; set; } = new RoomConfiguration();
+}
+
--- test/ChatRoom.Tests/Program.cs
+++ test/ChatRoom.Tests/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System.Management.Automation;
-using ChatRoom.BingSearch.Tests;
+using ChatRoom.WebSearch.Tests;
 using ChatRoom.Client.Tests;
 using ChatRoom.Github.Tests;
 using ChatRoom.OpenAI.Tests;
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Hosting;
 
 using var defaultClientFixture = new DefaultClientFixture();
 using var openaiFixture = new OpenAIAgentsFixture();
-using var bing = new BingSearchFixture();
+using var bing = new WebSearchFixture();
 using var githubFixture = new GithubAgentsFixture();
 using var psFixture = new PowershellAgentsFixture();
 using var host = Host.CreateDefaultBuilder()
